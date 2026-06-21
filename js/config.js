3f617ccecacd5d80d25b89f570a807c5ba4fe3ff
AdivaldoSan <professoradivaldo@gmail.com>
2026-06-20 21:20:49 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 window.API_URL =
-"https://script.google.com/macros/s/AKfycbzNhHeIb9ji4-mU_MsrfE48biEfi0cM2BwDmzF0phbR7qJo29kiXbxu53hjOtPd1fyzXg/exec";
+"https://script.google.com/macros/s/AKfycbzhJiuJj0Itu1y_FaI0o_kbIxnRcOj48nJysbj-SQS_WOy0ms6oKbfX5k0lVuUXd7_aiw/exec";
